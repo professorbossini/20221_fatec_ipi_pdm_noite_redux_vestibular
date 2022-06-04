f63d102e57d79b19d13041a38999e6d26ca5d1b3
Rodrigo Bossini <professorbossini@gmail.com>
2022-06-03 21:39:51 -0300

    apostila09(redux-exercicio-extra): fim da secao 7
    reducers implementados

--- index.js
+++ index.js
@@ -17,3 +17,20 @@ const realizarMatricula  = (cpf, status) => {
         payload: {cpf, status}
     }
 }
+
+//reducer manipula a seguinte lista
+//[{cpf: 1, nome: "Ana", nota: 10}, {cpf: 2, nome: "Ana2", nota: 10}]
+const historicoVestibular = (historicoVestibularAtual = [], acao) => {
+    if (acao.type === "REALIZAR_VESTIBULAR"){
+        return [...historicoVestibularAtual, acao.payload]
+    }
+    return historicoVestibularAtual
+}
+//reducer manipula a seguinte lista
+//[{cpf: 1, status: M}, {cpf: 2, status: M}, {cpf: 3, status: NM}]
+const historicoMatriculas = (historicoMatriculasAtual = [], acao) => {
+    if (acao.type === "REALIZAR_MATRICULA"){
+        return [...historicoMatriculasAtual, acao.payload]
+    }
+    return historicoMatriculasAtual
+}
